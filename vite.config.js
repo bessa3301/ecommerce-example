9698145fea9007235b0e70b3eff79ce2d37505a3
bessa3301 <bessa.3301@gmail.com>
2026-01-01 19:28:34 -0300

    add login-register boilerplate

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -19,6 +20,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
     server: {
         host: '0.0.0.0',
         port: 3001,
